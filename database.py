42c72c7e2910eead16e05fb777324719a61665c3
Yassine ELKEFI <74304085+YassineElkefi@users.noreply.github.com>
2024-04-30 11:50:13 +0100

    first commit

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Taraji_Dawla1919@la5-db-2.cb0i0ouu60of.us-east-1.rds.amazonaws.com/postgres"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
